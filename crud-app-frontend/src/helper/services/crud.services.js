9bd34a55c358b76a124c50088fd6897ee6dc9042
bachelizer <74942434+bachelizer@users.noreply.github.com>
2022-06-02 08:38:10 +0800

    Update crud.services.js

--- crud-app-frontend/src/helper/services/crud.services.js
+++ crud-app-frontend/src/helper/services/crud.services.js
@@ -6,7 +6,8 @@ const addInstructors = (data) => apiClient.post('/instructor', data);
 
 const updateInstructor = (data) => apiClient.put('/instructor', data);
 
-// 
+// add your code here..
+// API for deletion of instructor
 
 export default {
   fetchInstructors,
